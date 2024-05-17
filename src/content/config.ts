ec798770b3aec7026f5ca5207a3fe6c258388fca
samuelhulla <oficialny.hulla@gmail.com>
2024-05-17 16:04:53 +0200

    :screwdriver: config: docs configuration

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,12 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { CATEGORIES } from '@/lib/constants'
+import { defineCollection, z } from 'astro:content'
+
+const docsSchema = z.object({
+  title: z.string(),
+  category: z.enum(CATEGORIES).default('Getting Started'),
+  name: z.string().optional(),
+})
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-};
+  docs: defineCollection({ type: 'content', schema: docsSchema }),
+}
